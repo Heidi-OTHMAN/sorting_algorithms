8858a1296d483c206649770cf9f4828bb3c3144c
Heidi-OTHMAN <heidiothman355@gmail.com>
2024-02-19 14:47:26 -0800

    a

---
+++ 100-shell_sort.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+
+/**
+*swap - the positions of two elements into an array
+*@array: array
+*@item_1: array element
+*@item_2: array element
+*/
+void swap(int *array, int item_1, int item_2)
+{
+
+	int a;
+
+	a = array[item_1];
+	array[item_1] = array[item_2];
+	array[item_2] = a;
+}
+/**
+ * shell_sort - function that sorts an array of integers in ascending
+ * order using the Shell sort algorithm, using the Knuth sequence
+ * @size: size of the array
+ * @array: list with numbers
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gp = 1, l, idx = 0;
+
+	if (array == NULL || size < 2)
+		return;
+	while (gp < size / 3)
+		gp = 3 * gp + 1;
+	while (gp >= 1)
+	{
+		for (l = gp; l < size; l++)
+			for (idx = l; idx >= gp &&
+			 (array[idx] < array[idx - gp]); idx -= gp)
+				swap(array, idx, idx - gp);
+		print_array(array, size);
+		gp /= 3;
+	}
+}
